d536a4d3ba20197a065666162017875a56f3f371
Ayush Kathoke <113281600+Ayushkathoke@users.noreply.github.com>
2024-07-06 16:45:13 +0530

    finally done

---
+++ client/src/components/common/Footer.tsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+        <footer className="static bottom-1 left-0 flex w-full justify-center sm:fixed">
+            <span>
+                Build with ❤️ by{" "}
+                <a
+                    href="https://github.com/ayushkathoke"
+                    className="text-primary underline underline-offset-1"
+                >
+                    Ayush Kathoke
+                </a>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
